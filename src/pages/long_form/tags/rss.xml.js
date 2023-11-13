fdc6d2cb76794e9f00af803cbb6f28a0f483db4d
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-13 15:55:05 +0000

    v1.1.3.1.5 RSS for every collection

--- src/pages/long_form/tags/rss.xml.js
+++ src/pages/long_form/tags/rss.xml.js
@@ -5,23 +5,27 @@ import MarkdownIt from 'markdown-it';
 const parser = new MarkdownIt();
 
 export async function GET(context) {
-    const muses = await getCollection("muses");
-    const short_form = await getCollection("short_form");
     const long_form = await getCollection("long_form");
-    const zeitweilig = await getCollection("zeitweilig");
     return rss({
         stylesheet: '/rss/rss.xsl',
         title: 'Revista',
         description: 'Life in review. A recollection of things experienced, and things felt.',
         site: context.site,
-        items: muses.map((post) => ({
+        items: long_form.map((post) => ({
             title: post.data.title,
             pubDate: post.data.pubDate,
             description: post.data.description,
-            link: `/muses/${post.slug}/`,
+            link: `/long_form/tags/${post.slug}/`,
             content: sanitizeHtml(parser.render(post.body)),
             ...post.data,
         })),
+        customData: `<language>en-us</language>`,
+    });
+}
+
+    // const short_form = await getCollection("short_form");
+    // const long_form = await getCollection("long_form");
+    // const zeitweilig = await getCollection("zeitweilig");
         // short_form.map((post) => ({
         //     title: post.data.title,
         //     pubDate: post.data.pubDate,
@@ -40,6 +44,3 @@ export async function GET(context) {
         //     description: post.data.description,
         //     link: `/zeitweilig/${post.slug}/`,
         // })),
-        customData: `<language>en-us</language>`,
-    });
-}
---
+++ src/pages/zeitweilig/rss.xml.js
@@ -0,0 +1,46 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+    const zeitweilig = await getCollection("zeitweilig");
+    return rss({
+        stylesheet: '/rss/rss.xsl',
+        title: 'Revista',
+        description: 'Life in review. A recollection of things experienced, and things felt.',
+        site: context.site,
+        items: zeitweilig.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            link: `/zeitweilig/${post.slug}/`,
+            content: sanitizeHtml(parser.render(post.body)),
+            ...post.data,
+        })),
+        customData: `<language>en-us</language>`,
+    });
+}
+
+    // const short_form = await getCollection("short_form");
+    // const long_form = await getCollection("long_form");
+    // const zeitweilig = await getCollection("zeitweilig");
+        // short_form.map((post) => ({
+        //     title: post.data.title,
+        //     pubDate: post.data.pubDate,
+        //     description: post.data.description,
+        //     link: `/short_form/${post.slug}/`,
+        // })),
+        // long_form.map((post) => ({
+        //     title: post.data.title,
+        //     pubDate: post.data.pubDate,
+        //     description: post.data.description,
+        //     link: `/long_form/${post.slug}/`,
+        // })),
+        // zeitweilig.map((post) => ({
+        //     title: post.data.title,
+        //     pubDate: post.data.pubDate,
+        //     description: post.data.description,
+        //     link: `/zeitweilig/${post.slug}/`,
+        // })),
