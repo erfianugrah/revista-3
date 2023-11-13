3d17b02990ca7d05cfd3b67cdf0e82d515f61750
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-13 15:59:40 +0000

    v1.1.3.1.6 robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import { remarkModifiedTime } from './src/scripts/remark-modified-time.mjs';
 import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://erfianugrah.com",
-  integrations: [preact()],
+  integrations: [preact(), sitemap()],
   markdown: {
-    remarkPlugins: [remarkModifiedTime],
-  },
+    remarkPlugins: [remarkModifiedTime]
+  }
 });
