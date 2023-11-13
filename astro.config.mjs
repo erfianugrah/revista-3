b15d287904c9b1b37bdf8b573d55bd6688dc6e2f
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-13 16:06:41 +0000

    v1.1.3.1.7 mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { remarkModifiedTime } from './src/scripts/remark-modified-time.mjs';
 import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://erfianugrah.com",
-  integrations: [preact(), sitemap()],
+  integrations: [preact(), sitemap(), mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: { theme: 'dracula' },
+      gfm: false,
+    })],
   markdown: {
     remarkPlugins: [remarkModifiedTime]
   }
