2ae0a5284cf6cf64e3e44611fc183b1dbeb1ceb1
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-13 11:52:41 +0000

    added ron burgundy

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -8,9 +8,9 @@ export default function Greeting({messages}) {
 
   return (
     <div>
-      <h3>{greeting}! Thank you for visiting!</h3>
+      <h3>{greeting}</h3>
       <button onClick={() => setGreeting(randomMessage())}>
-        New Greeting
+        New Ron Burgundy Quote
       </button>
     </div>
   );
