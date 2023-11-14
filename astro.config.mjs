5843e51edc9b61a6c61fa1b176aea06f3416671f
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-14 14:54:26 +0000

    v1.2 - tailwind CSS masonry, typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,20 @@ import { defineConfig } from 'astro/config';
 import { remarkModifiedTime } from './src/scripts/remark-modified-time.mjs';
 import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://erfianugrah.com",
   integrations: [preact(), sitemap(), mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: { theme: 'dracula' },
-      gfm: false,
-    })],
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'dracula'
+    },
+    gfm: false
+  }), tailwind()],
   markdown: {
     remarkPlugins: [remarkModifiedTime]
   }
