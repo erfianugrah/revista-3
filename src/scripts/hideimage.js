7a0c9ac57bc2765fa3a7c7a617e960779bf862e8
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-20 11:58:19 +0800

    1.6 - removed some assets

--- src/scripts/hideimage.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('astro:page-load', () => {
-    document.addEventListener('DOMContentLoaded', function () {
-        let imgElement = document.querySelector('#image img');
-        if (imgElement.getAttribute('src') === "") {
-            imgElement.parentElement.remove();
-        }
-    });
-});
--- src/scripts/stoppropagation.js
+++
@@ -1,8 +0,0 @@
-
-
-
-document.addEventListener('astro:page-load', () => {
-    document.querySelector('a').addEventListener('click', function (event) {
-        event.stopPropagation();
-    });
-});
