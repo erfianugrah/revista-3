47c1446ca2496bf93b53c6c6562768c62cb2670c
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-22 16:39:07 +0800

    1.7.5 -- refactored to client side js

--- src/scripts/getrandomimage.js
+++ src/scripts/getrandomimage.js
@@ -1,4 +1,5 @@
-
-export function getRandomImage(images) {
-    return images[Math.floor(Math.random() * images.length)];
-}
+  document.addEventListener('astro:page-load', function() {
+    const images = JSON.parse(document.querySelector('.relative.group.border-0').dataset.images);
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    document.getElementById('randomImage').src = randomImage;
+  });
