113b147918eaa0f278841fb78d3a476826bcbe04
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-22 16:46:02 +0800

    1.7.5.2 -- refactor the names

--- src/scripts/getrandomimage.js
+++ src/scripts/getrandomimage.js
@@ -3,5 +3,5 @@
     const alt = JSON.parse(document.querySelector('.relative.group.border-0').dataset.alt); // Parse the alt texts
     const index = Math.floor(Math.random() * images.length); // Get a random index
     document.getElementById('randomImage').src = images[index]; // Use the random index to get an image
-    document.getElementById('randomImage').alt = altTexts[index]; // Use the same index to get an alt text
+    document.getElementById('randomImage').alt = alt[index]; // Use the same index to get an alt text
   });
