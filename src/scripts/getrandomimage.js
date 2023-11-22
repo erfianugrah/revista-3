1730e66d1cd8b69d8554583aeb0379cccfa76b95
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-22 16:44:03 +0800

    1.7.5.1 -- did the same for alt

--- src/scripts/getrandomimage.js
+++ src/scripts/getrandomimage.js
@@ -1,5 +1,7 @@
   document.addEventListener('astro:page-load', function() {
     const images = JSON.parse(document.querySelector('.relative.group.border-0').dataset.images);
-    const randomImage = images[Math.floor(Math.random() * images.length)];
-    document.getElementById('randomImage').src = randomImage;
+    const alt = JSON.parse(document.querySelector('.relative.group.border-0').dataset.alt); // Parse the alt texts
+    const index = Math.floor(Math.random() * images.length); // Get a random index
+    document.getElementById('randomImage').src = images[index]; // Use the random index to get an image
+    document.getElementById('randomImage').alt = altTexts[index]; // Use the same index to get an alt text
   });
