b5342debe63efefb565be2b1308f638acc1f7dc5
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-22 16:51:39 +0800

    1.7.5.3 -- adding fisher-yates shuffle

--- src/scripts/getrandomimage.js
+++ src/scripts/getrandomimage.js
@@ -1,7 +1,14 @@
-  document.addEventListener('astro:page-load', function() {
-    const images = JSON.parse(document.querySelector('.relative.group.border-0').dataset.images);
-    const alt = JSON.parse(document.querySelector('.relative.group.border-0').dataset.alt); // Parse the alt texts
-    const index = Math.floor(Math.random() * images.length); // Get a random index
-    document.getElementById('randomImage').src = images[index]; // Use the random index to get an image
-    document.getElementById('randomImage').alt = alt[index]; // Use the same index to get an alt text
-  });
+document.addEventListener('astro:page-load', function() {
+  let images = JSON.parse(document.querySelector('.relative.group.border-0').dataset.images);
+  let alt = JSON.parse(document.querySelector('.relative.group.border-0').dataset.alt); // Parse the alt texts
+
+  // Fisher-Yates shuffle
+  for (let i = images.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [images[i], images[j]] = [images[j], images[i]];
+    [alt[i], alt[j]] = [alt[j], alt[i]];
+  }
+
+  document.getElementById('randomImage').src = images[0]; // Use the first image
+  document.getElementById('randomImage').alt = alt[0]; // Use the first alt text
+});
