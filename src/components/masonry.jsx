2181062595c21654f7a9eeb7d83965ee75764e18
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-23 09:40:19 +0800

    1.7.7 -- just adde lazy loading to the gallery

--- src/components/masonry.jsx
+++ src/components/masonry.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-
 const MasonryLayout = ({ images }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {images.map((image, index) => (
         <div key={index} className="col-span-1">
           <a href={image.src} data-fslightbox="gallery">
-            <img src={image.src} alt={image.alt} />
+            <img src={image.src} alt={image.alt} loading="lazy" />
           </a>
         </div>
       ))}
