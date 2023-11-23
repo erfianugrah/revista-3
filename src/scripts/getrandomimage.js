ad8cbfe88302c0f46898596003d525e06aee3094
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-23 13:37:35 +0800

    1.7.8 -- polishing tag layout

--- src/scripts/getrandomimage.js
+++ src/scripts/getrandomimage.js
@@ -1,14 +1,18 @@
 document.addEventListener('astro:page-load', function() {
-  let images = JSON.parse(document.querySelector('.relative.group.border-0').dataset.images);
-  let alt = JSON.parse(document.querySelector('.relative.group.border-0').dataset.alt); // Parse the alt texts
+  let imageElements = document.querySelectorAll('.relative.group.border-0');
+  
+  imageElements.forEach((element) => {
+    let images = JSON.parse(element.dataset.images);
+    let alt = JSON.parse(element.dataset.alt); // Parse the alt texts
 
-  // Fisher-Yates shuffle
-  for (let i = images.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [images[i], images[j]] = [images[j], images[i]];
-    [alt[i], alt[j]] = [alt[j], alt[i]];
-  }
+    // Fisher-Yates shuffle
+    for (let i = images.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [images[i], images[j]] = [images[j], images[i]];
+      [alt[i], alt[j]] = [alt[j], alt[i]];
+    }
 
-  document.getElementById('randomImage').src = images[0]; // Use the first image
-  document.getElementById('randomImage').alt = alt[0]; // Use the first alt text
+    element.querySelector('img').src = images[0]; // Use the first image
+    element.querySelector('img').alt = alt[0]; // Use the first alt text
+  });
 });
