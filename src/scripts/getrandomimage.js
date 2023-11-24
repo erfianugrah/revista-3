8927b6b110451ad2ff56a1d36263f1873fb2ded2
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-24 10:46:11 +0800

    1.8.1 -- fixed js for multiple images

--- src/scripts/getrandomimage.js
+++ src/scripts/getrandomimage.js
@@ -1,29 +1,23 @@
-let shuffledImages = [];
-let shuffledAlts = [];
-let currentIndex = 0;
-
 document.addEventListener('astro:page-load', function() {
   let imageElements = document.querySelectorAll('.relative.group.border-0');
-  
+
   imageElements.forEach((element) => {
     let images = JSON.parse(element.dataset.images);
     let alt = JSON.parse(element.dataset.alt); // Parse the alt texts
 
-    // Shuffle the arrays if they haven't been shuffled yet
-    if (shuffledImages.length === 0) {
-      for (let i = images.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [images[i], images[j]] = [images[j], images[i]];
-        [alt[i], alt[j]] = [alt[j], alt[i]];
-      }
-      shuffledImages = images;
-      shuffledAlts = alt;
+    // Shuffle the arrays
+    for (let i = images.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      let tempImage = images[i];
+      let tempAlt = alt[i];
+      images[i] = images[j];
+      alt[i] = alt[j];
+      images[j] = tempImage;
+      alt[j] = tempAlt;
     }
 
-    element.querySelector('img').src = shuffledImages[currentIndex]; // Use the next image
-    element.querySelector('img').alt = shuffledAlts[currentIndex]; // Use the corresponding alt text
-
-    // Update the current index, wrapping around to the start of the array if necessary
-    currentIndex = (currentIndex + 1) % shuffledImages.length;
+    let imgElement = element.querySelector('img');
+    imgElement.src = images[0]; // Use the first image
+    imgElement.alt = alt[0]; // Use the corresponding alt text
   });
 });
