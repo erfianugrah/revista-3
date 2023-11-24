15535695386ef5cd8c234bf90c94864fb884a8da
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-24 08:26:50 +0800

    1.7.9.1 -- improve on shuffle efficiency

--- src/scripts/getrandomimage.js
+++ src/scripts/getrandomimage.js
@@ -1,3 +1,7 @@
+let shuffledImages = [];
+let shuffledAlts = [];
+let currentIndex = 0;
+
 document.addEventListener('astro:page-load', function() {
   let imageElements = document.querySelectorAll('.relative.group.border-0');
   
@@ -5,14 +9,21 @@ document.addEventListener('astro:page-load', function() {
     let images = JSON.parse(element.dataset.images);
     let alt = JSON.parse(element.dataset.alt); // Parse the alt texts
 
-    // Fisher-Yates shuffle
-    for (let i = images.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [images[i], images[j]] = [images[j], images[i]];
-      [alt[i], alt[j]] = [alt[j], alt[i]];
+    // Shuffle the arrays if they haven't been shuffled yet
+    if (shuffledImages.length === 0) {
+      for (let i = images.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [images[i], images[j]] = [images[j], images[i]];
+        [alt[i], alt[j]] = [alt[j], alt[i]];
+      }
+      shuffledImages = images;
+      shuffledAlts = alt;
     }
 
-    element.querySelector('img').src = images[0]; // Use the first image
-    element.querySelector('img').alt = alt[0]; // Use the first alt text
+    element.querySelector('img').src = shuffledImages[currentIndex]; // Use the next image
+    element.querySelector('img').alt = shuffledAlts[currentIndex]; // Use the corresponding alt text
+
+    // Update the current index, wrapping around to the start of the array if necessary
+    currentIndex = (currentIndex + 1) % shuffledImages.length;
   });
 });
