a5e67151bd005a14bae55904dd95713fd1bcb3d7
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-24 10:18:52 +0800

    1.7.9.2 -- added error handling to menu.js

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,10 @@
 document.addEventListener('astro:page-load', () => {
-  document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
-  });
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  if (hamburger && navLinks) {
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('expanded');
+    });
+  }
 });
