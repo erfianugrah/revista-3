b5244181fd698df65fc26cb0738df4219c8cd5b7
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-27 18:51:41 +0100

    1.9.4.7 -- peter link and better masonry

--- src/components/masonry.jsx
+++ src/components/masonry.jsx
@@ -3,8 +3,8 @@ const MasonryLayout = ({ images }) => {
     <div style={{columnCount: 4, columnGap: '1em'}}>
       {images.map((image, index) => (
         <div key={index} style={{breakInside: 'avoid', marginBottom: '1em'}}>
-          <a href={image.src} data-fslightbox="gallery">
-            <img src={image.src} alt={image.alt} loading="lazy" />
+          <a href={image.src} data-fslightbox="gallery" style={{display: 'inline-block', margin: 0, padding: 0}}>
+            <img src={image.src} alt={image.alt} loading="lazy" style={{display: 'block', margin: 0, padding: 0}} />
           </a>
         </div>
       ))}
