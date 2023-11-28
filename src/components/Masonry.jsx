c754b5f17d37fe36e9fd31893b517fdd1dc9c907
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-28 19:34:59 +0100

    v1.9.5.9  -- masonry to one row for mobile

--- src/components/Masonry.jsx
+++ src/components/Masonry.jsx
@@ -1,10 +1,12 @@
+import '../styles/MasonryLayout.css';
+
 const MasonryLayout = ({ images }) => {
   return (
-    <div style={{columnCount: 4, columnGap: '1em'}}>
+    <div className="masonry">
       {images.map((image, index) => (
-        <div key={index} style={{breakInside: 'avoid', marginBottom: '1em'}}>
-          <a href={image.src} data-fslightbox="gallery" style={{display: 'inline-block', margin: 0, padding: 0}}>
-            <img src={image.src} alt={image.alt} loading="lazy" style={{display: 'block', margin: 0, padding: 0}} />
+        <div key={index} className="image-container">
+          <a href={image.src} data-fslightbox="gallery" className="image-link">
+            <img src={image.src} alt={image.alt} loading="lazy" className="image" />
           </a>
         </div>
       ))}
