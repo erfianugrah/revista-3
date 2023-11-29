8476b0d682007e038d8a30dee5b7d72e49121e2a
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-29 10:25:15 +0100

    v1.9.6.6 -- added astro:page-load

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,14 +1,16 @@
-window.toggleMenu = function() {
-  const navLinks = document.getElementById('nav-links');
-  const hamburger = document.querySelector('.hamburger');
+document.addEventListener('astro:page-load', () => {
+  window.toggleMenu = function () {
+    const navLinks = document.getElementById('nav-links');
+    const hamburger = document.querySelector('.hamburger');
 
-  if (navLinks.classList.contains('open')) {
-    navLinks.classList.remove('open');
-    navLinks.classList.add('collapsed');
-  } else {
-    navLinks.classList.remove('collapsed');
-    navLinks.classList.add('open');
+    if (navLinks.classList.contains('open')) {
+      navLinks.classList.remove('open');
+      navLinks.classList.add('collapsed');
+    } else {
+      navLinks.classList.remove('collapsed');
+      navLinks.classList.add('open');
+    }
+
+    hamburger.classList.toggle('open');
   }
-
-  hamburger.classList.toggle('open');
-}
+});
