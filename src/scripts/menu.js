0bf92dbcd8ab97266535b98296bcc5b7eb9e51ba
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-29 10:22:42 +0100

    v1.9.6.5 -- made navigation a bit more smooth

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,8 +1,14 @@
-document.addEventListener("astro:page-load", () => {
 window.toggleMenu = function() {
   const navLinks = document.getElementById('nav-links');
   const hamburger = document.querySelector('.hamburger');
-  navLinks.classList.toggle('open');
+
+  if (navLinks.classList.contains('open')) {
+    navLinks.classList.remove('open');
+    navLinks.classList.add('collapsed');
+  } else {
+    navLinks.classList.remove('collapsed');
+    navLinks.classList.add('open');
+  }
+
   hamburger.classList.toggle('open');
 }
-});
