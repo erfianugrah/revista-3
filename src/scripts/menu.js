85a024878db77d8d7f6d852fd8ae2833d097d00e
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-11-30 07:25:24 +0000

    v1.9.9.2 -- using opacity instead

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,14 +3,13 @@ document.addEventListener('astro:page-load', () => {
     const navLinks = document.getElementById('nav-links');
     const hamburger = document.querySelector('.hamburger');
 
-    const isNavOpen = navLinks.classList.contains('open');
-    navLinks.classList.remove('open', 'collapsed');
-
-    requestAnimationFrame(() => {
-      requestAnimationFrame(() => {
-        navLinks.classList.add(isNavOpen ? 'collapsed' : 'open');
-      });
-    });
+    if (navLinks.classList.contains('open')) {
+      navLinks.classList.remove('open');
+      navLinks.classList.add('collapsed');
+    } else {
+      navLinks.classList.remove('collapsed');
+      navLinks.classList.add('open');
+    }
 
     hamburger.classList.toggle('open');
   }
