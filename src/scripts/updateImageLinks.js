5d7664c3691fc2bb81fc1c6dbae149588f95af38
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-12-03 09:17:30 +0000

    v2.1.7.8 -- this is supposed to work, let's try again

--- src/scripts/updateImageLinks.js
+++ src/scripts/updateImageLinks.js
@@ -1,26 +1,12 @@
 document.addEventListener('astro:page-load', () => {
-    // Create a new script element for the fslightbox.js script
-    const fslightboxScript = document.createElement('script');
-    fslightboxScript.src = './fslightbox.js';
+    const imageContainers = document.querySelectorAll('.masonry .image-container');
 
-    // Add an event listener for the load event of the fslightbox.js script
-    fslightboxScript.addEventListener('load', () => {
-        const imageContainers = document.querySelectorAll('.masonry .image-container');
+    imageContainers.forEach(container => {
+        const imageLink = container.querySelector('.image-link');
+        const image = container.querySelector('.image');
 
-        imageContainers.forEach(container => {
-            const imageLink = container.querySelector('.image-link');
-            const image = container.querySelector('.image');
-
-            if (imageLink && image) {
-                imageLink.href = image.src;
-            }
-        });
-
-        if (window.refreshFsLightbox) {
-            window.refreshFsLightbox();
+        if (imageLink && image) {
+            imageLink.href = image.src;
         }
     });
-
-    // Append the fslightbox.js script to the document
-    document.body.appendChild(fslightboxScript);
 });
