a8778d241e5a3fc53388ff9994f31f39497eaac3
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-12-03 09:03:48 +0000

    v2.1.7.6 -- script didn't run

--- src/scripts/updateImageLinks.js
+++ src/scripts/updateImageLinks.js
@@ -1,12 +1,26 @@
 document.addEventListener('astro:page-load', () => {
-    const imageContainers = document.querySelectorAll('.masonry .image-container');
+    // Create a new script element for the fslightbox.js script
+    const fslightboxScript = document.createElement('script');
+    fslightboxScript.src = '../scripts/fslightbox.js';
 
-    imageContainers.forEach(container => {
-        const imageLink = container.querySelector('.image-link');
-        const image = container.querySelector('.image');
+    // Add an event listener for the load event of the fslightbox.js script
+    fslightboxScript.addEventListener('load', () => {
+        const imageContainers = document.querySelectorAll('.masonry .image-container');
 
-        if (imageLink && image) {
-            imageLink.href = image.src;
+        imageContainers.forEach(container => {
+            const imageLink = container.querySelector('.image-link');
+            const image = container.querySelector('.image');
+
+            if (imageLink && image) {
+                imageLink.href = image.src;
+            }
+        });
+
+        if (window.refreshFsLightbox) {
+            window.refreshFsLightbox();
         }
     });
+
+    // Append the fslightbox.js script to the document
+    document.body.appendChild(fslightboxScript);
 });
