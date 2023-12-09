4ffbac07435dbdd53d807e77378ef6081c768a72
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-12-09 19:59:02 +0100

    v2.3.3.4 -- lightbox not showing close button on firefox mobile

--- src/scripts/lightbox.js
+++ src/scripts/lightbox.js
@@ -3,6 +3,7 @@ import GLightbox from 'glightbox';
 document.addEventListener('astro:page-load', function () {
     const lightbox = GLightbox({
         touchNavigation: true,
+        closeButton: true,
         loop: true,
         width: "85vw",
         height: "85vh",
