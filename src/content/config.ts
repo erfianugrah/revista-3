f6770cd9e11b7445e5b1868e1eb3324615f6f6e8
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-12-10 19:00:07 +0100

    v2.3.6.2 -- config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -66,6 +66,22 @@ const zeitweilig = defineCollection({
   }),
 });
 
+const authors = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    author: z.string(),
+    description: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }).optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
+
 
 // 3. Export a single `collections` object to register your collection(s)
-export const collections = { muses, short_form, long_form, zeitweilig, };
+export const collections = { muses, short_form, long_form, zeitweilig, authors };
