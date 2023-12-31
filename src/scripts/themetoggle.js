e68bbdcd47e5f22bcbdb9e1342d9eaf6f69ddedd
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-12-31 07:31:36 +0000

    v2.5 -- again

--- src/scripts/themetoggle.js
+++ src/scripts/themetoggle.js
@@ -1,8 +1,22 @@
-document.addEventListener('astro:page-load', () => {
-    // Remove all theme classes
-    document.documentElement.classList.remove("light", "dark");
+// Check if the theme is stored in localStorage
+let theme = localStorage.getItem("theme");
 
-    // Add the current theme class
+// If not, check the user's system preference
+if (!theme) {
+  theme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+}
+
+// Set the theme in localStorage
+localStorage.setItem("theme", theme);
+
+// Add the theme as a class on the html element
+document.documentElement.classList.add(theme);
+
+document.addEventListener('astro:page-load', () => {
+    // Get the theme from localStorage
+    theme = localStorage.getItem("theme");
+
+    // Add the theme as a class on the html element
     document.documentElement.classList.add(theme);
 
     document.getElementById("themeToggle").addEventListener("click", () => {
