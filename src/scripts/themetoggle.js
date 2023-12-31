26995be061fc64f1ce74ce005facbdbee96840c0
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-12-31 07:51:58 +0000

    v2.5.1.1 -- trying before-swap

--- src/scripts/themetoggle.js
+++ src/scripts/themetoggle.js
@@ -12,13 +12,15 @@ localStorage.setItem("theme", theme);
 // Add the theme as a custom attribute on the html element
 document.documentElement.setAttribute('data-theme', theme);
 
-document.addEventListener('astro:page-load', () => {
+document.addEventListener('astro:before-swap', () => {
     // Get the theme from localStorage
     theme = localStorage.getItem("theme");
 
     // Add the theme as a custom attribute on the html element
     document.documentElement.setAttribute('data-theme', theme);
+});
 
+document.addEventListener('astro:page-load', () => {
     document.getElementById("themeToggle").addEventListener("click", () => {
         // Toggle the theme
         theme = document.documentElement.getAttribute('data-theme') === "dark" ? "light" : "dark";
