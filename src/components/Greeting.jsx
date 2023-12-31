2df261b5ea4827811f2b10870be700f4cd2b7339
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-12-31 16:03:09 +0000

    v2.5.4.4 -- comment out jsx

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'preact/hooks';
+// import { useState } from 'preact/hooks';
 
-export default function Greeting({ messages }) {
+// export default function Greeting({ messages }) {
 
-  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+//   const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
 
-  const [greeting, setGreeting] = useState(messages[0]);
+//   const [greeting, setGreeting] = useState(messages[0]);
 
-  return (
-    <div>
-      <h3 class="min-h-[250px] flex min-w-0 overflow-auto">{greeting}</h3>
-      <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={() => setGreeting(randomMessage())}>
-        Generate Random Ron Burgundy Quote
-      </button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h3 class="min-h-[250px] flex min-w-0 overflow-auto">{greeting}</h3>
+//       <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={() => setGreeting(randomMessage())}>
+//         Generate Random Ron Burgundy Quote
+//       </button>
+//     </div>
+//   );
+// }
