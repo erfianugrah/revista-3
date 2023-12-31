2fd76d9604ba3ffa07d2abbe7fbd421aa6aca7b2
Erfi Anugrah <erfi.anugrah@gmail.com>
2023-12-31 09:52:07 +0000

    v2.5.2.8 -- wrap click even in astro:page-load

--- src/scripts/themetoggle.js
+++ src/scripts/themetoggle.js
@@ -12,18 +12,20 @@ localStorage.setItem("theme", theme);
 // Add the theme as a class on the html element
 document.documentElement.classList.add(theme);
 
-document.getElementById("themeToggle").addEventListener('astro:page-load', () => {
-  // Toggle the .dark class
-  document.documentElement.classList.toggle("dark");
+document.addEventListener('astro:page-load', () => {
+  document.getElementById("themeToggle").addEventListener('click', () => {
+    // Toggle the .dark class
+    document.documentElement.classList.toggle("dark");
 
-  // Check if the .dark class is present
-  const isDark = document.documentElement.classList.contains("dark");
+    // Check if the .dark class is present
+    const isDark = document.documentElement.classList.contains("dark");
 
-  // Set the theme based on whether the .dark class is present
-  theme = isDark ? "dark" : "light";
+    // Set the theme based on whether the .dark class is present
+    theme = isDark ? "dark" : "light";
 
-  // Update the theme in localStorage
-  localStorage.setItem("theme", theme);
+    // Update the theme in localStorage
+    localStorage.setItem("theme", theme);
+  });
 });
 
 document.addEventListener('astro:after-swap', () => {
