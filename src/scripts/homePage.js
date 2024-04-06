6eb92f07b6246f57c42dc068d90f31ad3ac1c7b9
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-04-06 14:13:15 +0000

    test optimisation again

--- src/scripts/homePage.js
+++ src/scripts/homePage.js
@@ -1,23 +1,22 @@
+// document.addEventListener('astro:after-swap', handlePageLoad);
 function handlePageLoad() {
-  let imageElements = Array.from(document.querySelectorAll('#homepage'));
+  let imageElements = Array.from(document.querySelectorAll("#homepage"));
 
   imageElements.forEach((element) => {
-    let images = JSON.parse(element.dataset.images);
-    let alt = JSON.parse(element.dataset.alt); // Parse the alt texts
-    let urls = JSON.parse(element.dataset.urls); // Parse the urls
-    let width = element.dataset.width; // Get the width
-    let height = element.dataset.height; // Get the height
+    let { images, alt, urls } = JSON.parse(element.dataset);
+    let width = element.dataset.width;
+    let height = element.dataset.height;
 
-    // Combine the images, alt texts, and URLs into a single array of objects
-    let items = images.map((image, index) => {
-      return {
-        image: image,
+    let items = images.reduce((accumulator, image, index) => {
+      accumulator.push({
+        image,
         alt: alt[index],
         url: urls[index],
-        width: width,
-        height: height
-      };
-    });
+        width,
+        height,
+      });
+      return accumulator;
+    }, []);
 
     // Shuffle the items array
     for (let i = items.length - 1; i > 0; i--) {
@@ -25,19 +24,18 @@ function handlePageLoad() {
       [items[i], items[j]] = [items[j], items[i]];
     }
 
-    let imgElement = element.querySelector('img');
-    let anchorElement = element.querySelector('a');
+    let imgElement = element.querySelector("img");
+    let anchorElement = element.querySelector("a");
 
-    // Use the first item
-    let firstItem = items[0];
-    imgElement.src = firstItem.image;
-    imgElement.alt = firstItem.alt;
-    imgElement.width = firstItem.width; // Set the width
-    imgElement.height = firstItem.height; // Set the height
-    anchorElement.href = firstItem.url;
-    imgElement.classList.remove('hidden'); // Remove the 'hidden' class
+    Object.assign(imgElement, {
+      src: items[0].image,
+      alt: items[0].alt,
+      width: items[0].width,
+      height: items[0].height,
+    });
+    anchorElement.href = items[0].url;
+    imgElement.classList.remove("hidden");
   });
 }
 
-document.addEventListener('astro:page-load', handlePageLoad);
-// document.addEventListener('astro:after-swap', handlePageLoad);
+document.addEventListener("astro:page-load", handlePageLoad);
