45552aa2360c65a8ce3cdc072ac4ce8bf0e6ecd1
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-08-05 07:23:29 +0000

    v3.6.2 -- workflow updates part 30

--- analyse-commits.js
+++ analyse-commits.js
@@ -1,14 +1,28 @@
-const { execSync } = require('child_process');
+const { spawn } = require('bun');
+const semver = require('semver');
 
-try {
-  const currentTag = execSync(
-    'curl -s "https://hub.docker.com/v2/repositories/erfianugrah/revista-4/tags/?page_size=1&ordering=last_updated" | jq -r \'.results[0].name\''
-  )
-    .toString()
-    .trim();
-  console.log(`Current Docker tag: ${currentTag}`);
-  process.exit(0);
-} catch (error) {
-  console.error('Error fetching current Docker tag:', error);
-  process.exit(1);
+async function getCurrentTag() {
+  const proc = spawn([
+    'curl',
+    '-s',
+    'https://hub.docker.com/v2/repositories/erfianugrah/revista-4/tags/?page_size=1&ordering=last_updated',
+  ]);
+  const output = await new Response(proc.stdout).text();
+  const data = JSON.parse(output);
+  return data.results[0].name;
 }
+
+async function main() {
+  const lastReleaseVersion = process.argv[2];
+  const currentTag = await getCurrentTag();
+
+  console.log(`Last release version: ${lastReleaseVersion}`);
+  console.log(`Current Docker tag: ${currentTag}`);
+
+  const higherVersion = semver.gt(currentTag, lastReleaseVersion) ? currentTag : lastReleaseVersion;
+
+  console.log(`Using version: ${higherVersion}`);
+  console.log(`recommend-version:${higherVersion}`);
+}
+
+main().catch(console.error);
--- verify-conditions.js
+++ verify-conditions.js
@@ -1,17 +1,22 @@
-const { execSync } = require('child_process');
-const semver = require('semver');
+const { spawn } = require('bun');
 
-const lastReleaseVersion = process.argv[2];
-const currentTag = execSync(
-  'curl -s "https://hub.docker.com/v2/repositories/erfianugrah/revista-4/tags/?page_size=1&ordering=last_updated" | jq -r \'.results[0].name\''
-)
-  .toString()
-  .trim();
+async function getCurrentTag() {
+  const proc = spawn([
+    'curl',
+    '-s',
+    'https://hub.docker.com/v2/repositories/erfianugrah/revista-4/tags/?page_size=1&ordering=last_updated',
+  ]);
+  const output = await new Response(proc.stdout).text();
+  const data = JSON.parse(output);
+  return data.results[0].name;
+}
 
-console.log(`Last release version: ${lastReleaseVersion}`);
-console.log(`Current Docker tag: ${currentTag}`);
-
-const higherVersion = semver.gt(currentTag, lastReleaseVersion) ? currentTag : lastReleaseVersion;
-
-console.log(`Using version: ${higherVersion}`);
-console.log(`recommend-version:${higherVersion}`);
+getCurrentTag()
+  .then((currentTag) => {
+    console.log(`Current Docker tag: ${currentTag}`);
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error('Error fetching current Docker tag:', error);
+    process.exit(1);
+  });
