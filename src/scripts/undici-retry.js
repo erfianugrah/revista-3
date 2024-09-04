4973e3cd9a0d7f9bee984a62170ab2f416d2b89b
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-09-04 14:00:21 +0000

    v4.3.3 -- added undici retries

--- src/scripts/undici-retry.js
+++ src/scripts/undici-retry.js
@@ -1,11 +1,11 @@
-import { setGlobalDispatcher, Agent } from 'undici';
+import { setGlobalDispatcher, Agent, RetryAgent } from 'undici';
 
 export default () => ({
   name: 'undici-retry',
   hooks: {
     'astro:build:start': () => {
       // Increase connect timeout for HTTP requests
-      setGlobalDispatcher(new Agent({ connect: { timeout: 30000 } }));
+      setGlobalDispatcher(new RetryAgent(new Agent({ connect: { timeout: 30000 } })));
     },
   },
 });
