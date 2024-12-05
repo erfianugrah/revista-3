3c4da1d596067d20bf2c19bbfff660d2f4d9fb0c
Erfi Anugrah <erfi.anugrah@gmail.com>
2024-12-05 13:26:52 +0000

    v4.9.6 -- refactored for v5 part 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -63,7 +63,7 @@ export default defineConfig({
   },
   experimental: {
     clientPrerender: true,
-    responsiveImages: true,
+    // responsiveImages: true,
     // directRenderScript: true
   },
   build: {
