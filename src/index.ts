4178ff555c4501f1e6a2a02ce102d9d3d6f4bffa
Erfi Anugrah <erfi.anugrah@gmail.com>
2026-03-19 17:50:13 +0100

    feat: add www to apex 301 redirect in worker

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,14 @@ interface ExportedHandler<E = unknown> {
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
+    const url = new URL(request.url);
+
+    // Redirect www -> apex with a 301 (permanent)
+    if (url.hostname === "www.erfianugrah.com") {
+      url.hostname = "erfianugrah.com";
+      return Response.redirect(url.toString(), 301);
+    }
+
     return env.ASSETS.fetch(request);
   },
 } satisfies ExportedHandler<Env>;
