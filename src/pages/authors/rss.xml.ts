dd5cb4b8a7d208f64a95cc5743d111291433a48a
lpongan <lpongan@gmail.com>
2026-05-05 13:52:54 -0400

    remove demo content

--- src/pages/authors/rss.xml.ts
+++
@@ -1,8 +0,0 @@
-import { getCollection } from "astro:content";
-import { generateRss } from "../../scripts/collections";
-import type { APIContext } from "astro";
-
-export async function GET(context: APIContext) {
-  const entries = await getCollection("authors");
-  return generateRss(entries, "authors", context);
-}
--- src/pages/muses/rss.xml.ts
+++
@@ -1,8 +0,0 @@
-import { getCollection } from "astro:content";
-import { generateRss } from "../../scripts/collections";
-import type { APIContext } from "astro";
-
-export async function GET(context: APIContext) {
-  const entries = await getCollection("muses");
-  return generateRss(entries, "muses", context);
-}
